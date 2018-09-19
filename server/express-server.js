5a1fec5f1e87a1db98976354038e620ea654d90d
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-09-19 06:06:10 -0600

    Initial Checkin

---
+++ server/express-server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const contextRequire = require.context('./routes/', true, /\.js$/);
+contextRequire.keys().forEach(file => {
+  const fileRouter = contextRequire(file);
+  app.use('/', fileRouter.default || fileRouter);
+});
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const PORT = 8080;
+const http = require('http');
+
+const server = http.createServer(require('./express-server'));
+let currentExpressServer = require('./express-server');
+
+console.log(`App listening on port ${PORT}!\n`);
+server.listen(PORT);
+
+if (module.hot) {
+  module.hot.accept('./express-server', () => {
+    server.removeListener('request', currentExpressServer);
+    server.on('request', require('./express-server'));
+    currentExpressServer = require('./express-server');
+  });
+}
